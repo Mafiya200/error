fed2e35821d5c187cb1484f403a8369b6a3eaba7
Mafiya200 <64405517+Mafiya200@users.noreply.github.com>
2023-03-29 09:45:20 +0300

    Add files via upload

--- source/JS/script.js
+++ source/JS/script.js
@@ -1,5 +1,7 @@
 'use strict';
 
+///Сделай функцию, которая принимает массив любых целых чисел,
+/// которая возращает новый массив, где все элементы кратны пяти. ([1,2,5,12,15,21] вернет [5,15])
 const isMobile = {
     Android: function () {
         return navigator.userAgent.match(/Android/i);
@@ -26,140 +28,15 @@ const isMobile = {
     }
 };
 
-let sliderImage = new Swiper(`.swiper`, {
-    // стрелки
-    navigation: {
-        nextEl: `.image-slider-next.swiper-button-next`,
-        prevEl: `.image-slider-prev.swiper-button-prev`,
-    },
-    /*  pagination: {
-         el: `.swiper-pagination`,
-          type:`bullets`,
-          clickable: true,
-          dynamicBullets: true,
-  
-          renderBullet: function (index, className) {
-              return `<span class="` + className + `">` + (index + 1) + `</span>`;
-          } ,
-         // фракции
-           type:'fraction', 
-          renderFraction:function(currentClass,totalClass){
-              return `Фото <span class="` + currentClass + `"></span>` +
-               ` из ` + `<span class=` + totalClass +
-                `"></span`;
-          }, 
-         // Прогрессбар
-          type: `progressbar`, 
-     }, */
-    
-    simulateTouch: true,
-    touchRatio: 1,
-    grabCursor: true,
-
-    slideToClickedSlide: false,
-    /* hashNavigation: {
-        watchState: true,
-    }, */
-    keyboard: {
-        enable: true,
-        onlyInViewport: true,
-        pageUpDown: true,
-    },
-     mousewheel: { //прокрутка колесом мыши
-        sensitivity: 1,
-/*          eventsTarget: ".image-slider",
- */      }, 
-    autoHeight: false, // слайдер подстраивается под высоту контента
-    slidesPerView: 3, /// количество слайдом на показ
-    watchOverflow: true, //если слайдов меньше чем указано slidesPerView откличает слайдер
-    spaceBetween: 20, //Пространство между слайдами
-    slidesPerGroup: 1, // количество пролистываемых слайдов
-    /* centeredSlides:true, */ // делает слайд по центру
-    initialSlide: 1, // стартовый слайд
-    /* grid: {
-        rows: 1,
-    }, *///мультирядность
-    loop: true,// бесконечность
-    loopedSlides: 3, //кол-во дублирующих слайдов нужно методом тыка вручную узнать нужное значение
-    freeMode: false, //включает режим прокрутки без фиксация
-
-    /* autoplay:{
-    delay:1000,
-    stopOnLastSlide:true,
-        disableOnInteraction:false,
-    
-    }, */
-    speed: 800, // скорость прокрутки
-    /* direction:`vertical`, */ // вкл вертикальный режим прокрутки
-
-    effect: `slide`, // эффекты переключения слайдов
-    /* coverflowEffect:{// дополнение к fade
-    rotate:20,
-    stretch:50,
-    slideShadows:true,
-    }, */
-
-
-    breakpoints: {///адаптив
-        280: {
-            slidesPerView: 1,
-        },
-        480: {
-            slidesPerView: 2,
-        },
-        992: {
-            slidesPerView: 3,
-        },
-    },
-    preloadImages: false,
-    watchSlidesProgress: true,// слежка за видимыми слайдами         для значения slidesPerView auto или больше 1
-    watchSlidesVisibility: true,// добавление класса видимым слайдам для значения slidesPerView auto или больше 1
-
-    /* zoom:{
-        maxRatio:5,// максимальное увеличение
-        minRatio:1,// минимальное увеличение
-    } */
-
-});
-
-let swiper2 = new Swiper(`.image-in-slider`, {
-    grabCursor: true,
+const mySlider = new Swiper(`.lorem-dolor`, {
     pagination: {
-        el: `.swiper-pagination`,
-        clickable: true,
+        el: `.lorem-dolor__pagination`,
+        clickable:true,
+       
+        type: `bullets`,
     },
-    spaceBetween:1,
-    navigator:true,
-    nested:true, // корректная работа перетаскивания\свайпа для дочернего элемента
+    effect: `fade`,
+    /* дополнение к effect:`fade` == подходит для 1 слайда на странице*/
+    loop: true,
+   spaceBetween:30,
 });
-
-
-
-const textSlider = new Swiper(`.text-slider`,{
-slidesPerView:1,
-spaceBetween:20,
-effect: `slide`,
-speed:800,
-breakpoints: {///адаптив
-    280: {
-        slidesPerView: 1,
-    },
-    480: {
-        slidesPerView: 2,
-    },
-    992: {
-        slidesPerView: 3,
-    },
-},
-slideToClickedSlide: false,
-
-initialSlide: 1,
-loop:true,
-loopedSlides: 3,
-freeMode: false,
-});
-
-
-
-sliderImage.controller.control = textSlider;
-textSlider.controller.control = sliderImage;
